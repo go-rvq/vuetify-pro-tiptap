76545192f1a17e770ece8cd2b758ff49237df0fc
Moises P. Sena <moisespsena@gmail.com>
2025-12-02 11:22:44 +0000

    chore: add label to image extension

---
+++ src/extensions/components/image/ImageProperties.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import type { ImageForm } from './types'
+
+import { computed } from 'vue'
+
+import { getIcon } from '@/constants/icons'
+
+interface Props {
+  modelValue?: ImageForm
+  t: (path: string) => string
+}
+
+interface Emits {
+  (event: 'update:modelValue', value: ImageForm): void
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  modelValue: () => ({})
+})
+
+const emit = defineEmits<Emits>()
+
+const form = computed({
+  get: () => props.modelValue,
+  set: val => emit('update:modelValue', val)
+})
+</script>
+
+<template>
+  <VTextField
+    v-model="form.alt" :label="t('editor.image.dialog.form.alt')"
+    :prepend-icon="getIcon('text')"
+  />
+  <VCheckbox
+    v-model="form.lockAspectRatio" :label="t('editor.image.dialog.form.aspectRatio')"
+    hide-details
+  />
+  <VCheckbox
+    v-model="form.labelDisabled" :label="t('editor.image.dialog.form.labelDisabled')"
+    hide-details
+  />
+  <VTextField
+    v-if="!form.labelDisabled" v-model="form.label" hide-details
+    :label="t('editor.image.dialog.form.label')"
+    :prepend-icon="getIcon('imageLabel')"
+  />
+</template>
